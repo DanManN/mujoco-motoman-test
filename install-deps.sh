fab2766d2154a6622e4bc28c7a068614e6bf795e
Daniel Nakhimovich <danielnak@tutanota.com>
2023-01-15 21:01:41 +0200

    ubuntu deps

---
+++ install-deps.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+sudo apt install \
+	g++ \
+	swig \
+	patchelf \
+	python3-dev \
+	libglew-dev \
+	libnlopt-dev \
+	libeigen3-dev \
+	libomesa6-dev \
+	liburdfdom-dev \
+	libfreetype6-dev \
+	liborocos-kdl-dev \
+	libkdl-parser-dev
