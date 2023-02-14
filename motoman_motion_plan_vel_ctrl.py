8dbc465cc9b48acd625436b49ccead7e8cc49c4b
Daniel Nakhimovich <danielnak@tutanota.com>
2023-02-14 20:03:52 +0200

    plot trajectroy tracking

--- motoman_motion_plan_vel_ctrl.py
+++ motoman_motion_plan_vel_ctrl.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import seaborn as sb
+import matplotlib.pyplot as plt
+
 from init_scene import *
 
 robot_xml = './motoman/motoman_vc.xml'
@@ -8,6 +12,9 @@ scene_json = 'scene_shelf1.json'
 t0 = time.time()
 physics, viewer = init(robot_xml, assets_dir, scene_json)
 world, data = physics.model._model, physics.data._data
+
+viewer = mujoco_viewer.MujocoViewer(world, data)
+
 qinds = get_qpos_indices(world)
 
 dt = 0.001
@@ -17,7 +24,6 @@ ik_solver = TracIKSolver(
     "base_link",
     "motoman_left_ee",
 )
-iqpos = get_qpos_indices(world, ["sda10f/" + j for j in ik_solver.joint_names])
 lctrl = get_ctrl_indices(world, ["sda10f/" + j for j in ik_solver.joint_names])
 vctrl = get_ctrl_indices(world, ["sda10f/v_" + j for j in ik_solver.joint_names])
 # vctrl = get_ctrl_indices(world, ["sda10f/" + j for j in ik_solver.joint_names], 'v_')
@@ -40,27 +46,23 @@ print("total init:", t1 - t0)
 
 ## IK for target position
 ee_pose = tf.euler_matrix(-np.pi / 2, 0, -np.pi / 2)
-ee_pose[:3, 3] = world.body("btarget").pos - [0.05, 0, 0]
+ee_pose[:3, 3] = world.body("btarget").pos
 print(ee_pose)
 t0 = time.time()
 qout = ik_solver.ik(ee_pose, qinit=np.zeros(ik_solver.number_of_joints))
-ee_pose[:3, 3] += [0.05, 0, 0]
-qout2 = ik_solver.ik(ee_pose, qinit=qout)
 t1 = time.time()
 print("ik:", t1 - t0, qout)
 
 ## Motion plan to joint goal
 start = np.zeros(len(lctrl))
 goal = qout
-goal2 = qout2
 t0 = time.time()
 raw_plan = planner.plan(start, goal, 5, og.RRTConnect)
 t1 = time.time()
 print("motion plan:", t1 - t0, len(raw_plan))
-raw_plan.append(goal2)
 
 ## velocity control
-speed = 30
+speed = 20
 vel_limit = [-speed * np.pi / 180, speed * np.pi / 180]
 acc_limit = [-500. * np.pi / 180, 500. * np.pi / 180]
 import toppra as ta
@@ -94,21 +96,44 @@ mujoco.mj_forward(world, data)
 ## run simulation
 i = 0
 t = 0
+planned_traj = []
+real_traj = []
+time_ax = []
 while viewer.is_alive:
     # data.ctrl[lctrl] = pos_traj[i]
     # data.act[actrl] = pos_traj[i]
     # data.ctrl[vctrl] = vel_traj[i]
-    data.ctrl[lctrl] = jnt_traj(t)
-    data.ctrl[vctrl] = jnt_traj(t, 1)
+    q = jnt_traj(t)
+    dq = jnt_traj(t, 1)
+    data.ctrl[lctrl] = q
+    data.ctrl[vctrl] = dq
     mujoco.mj_step(world, data)
     t += dt
     if t > jnt_traj.duration:
         t = jnt_traj.duration
     else:
-        print(np.linalg.norm(data.qpos[iqpos] - data.ctrl[lctrl]))
+        # print(np.linalg.norm(data.qpos[qindl] - data.ctrl[lctrl]))
+        planned_traj.append(q)
+        real_traj.append(data.qpos[qindl])
+        time_ax.append(t - dt)
     if i < len(ts_sample) - 1:
         i += 1
 
     viewer.render()
 
 viewer.close()
+
+data = {}
+for p, r, t in zip(planned_traj, real_traj, time_ax):
+    for i in range(len(p)):
+        data[f'plan_{i}'] = data.get(f'plan_{i}', []) + [p[i]]
+        data[f'real_{i}'] = data.get(f'real_{i}', []) + [r[i]]
+    data['time'] = data.get('time', []) + [t]
+
+data = pd.DataFrame.from_dict(data)
+data = pd.melt(data, ['time'])
+data['type'] = [s[:-2] for s in data['variable']]
+data['color'] = [s[-1] for s in data['variable']]
+print(data)
+ax = sb.lineplot(data, x='time', y='value', hue='color', style='type')
+plt.show()
